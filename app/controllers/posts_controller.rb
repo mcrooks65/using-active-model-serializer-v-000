e1dcd2eebe4d4063a465aa2fe07d8bb2623572c4
Matthew Crooks <mfcrooks@gmail.com>
2018-11-13 02:13:17 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,8 +8,7 @@ class PostsController < ApplicationController
 
   def show
     @post = Post.find(params[:id])
-    render json: @post.to_json(only: [:title, :description, :id],
-                              include: [author: { only: [:name]}])
+    render json: @post
   end
 
   def new
