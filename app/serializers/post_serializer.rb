d2a92fe524a058e2d82c33059c39ee6afffee316
Matthew Crooks <mfcrooks@gmail.com>
2018-11-13 02:23:44 +0000

    automatically backed up by learn

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,4 +1,4 @@
 class PostSerializer < ActiveModel::Serializer
   attributes :id, :title, :description
-  belongs_to :author, serializer:
+  belongs_to :author, serializer: PostAuthorSerializer
 end
