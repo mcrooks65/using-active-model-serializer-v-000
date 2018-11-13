9b5784b54e763b7bfcb2bbe2f493849ad4335d5c
Matthew Crooks <mfcrooks@gmail.com>
2018-11-13 02:26:07 +0000

    automatically backed up by learn

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
 describe 'navigate' do
-  # before do
-  #   @post = Post.create(title: "My Post", description: "My post desc")
-  # end
+  before do
+    @post = Post.create(title: "My Post", description: "My post desc")
+  end
 
   it 'shows the title on the show page in a h1 tag' do
     visit post_path(@post)
